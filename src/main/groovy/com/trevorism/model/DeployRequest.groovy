c6835a9ace14ba810bffa3eae708a5d2cf257d29
Trevor Brooks <trevorism@gmail.com>
2024-05-26 21:12:56 -0400

    Remove security for the timebeing on create

--- src/main/groovy/com/trevorism/model/DeployRequest.groovy
+++ src/main/groovy/com/trevorism/model/DeployRequest.groovy
@@ -3,4 +3,10 @@ package com.trevorism.model
 class DeployRequest {
 
     String serviceName
+    String destinationPlatform
+    String destinationProject
+    String destinationEnvironment
+    String version
+    Map<String,String> configuration
+    List<String> secrets
 }
