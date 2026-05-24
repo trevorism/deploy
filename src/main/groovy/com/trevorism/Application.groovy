e2e9750fd8699dddf4a4e5d88e93c20abbdaf360
Trevor Brooks <trevorism@gmail.com>
2026-05-24 10:19:57 -0400

    update to java 25, micronaut 5

--- src/main/groovy/com/trevorism/Application.groovy
+++ src/main/groovy/com/trevorism/Application.groovy
@@ -9,7 +9,7 @@ import io.swagger.v3.oas.annotations.info.Info
 @OpenAPIDefinition(
         info = @Info(
                 title = "Deploy",
-                version = "0.3.0",
+                version = "0.4.0",
                 description = "Deploy of Trevorism",
                 contact = @Contact(url = "https://trevorism.com", name = "Trevor Brooks", email = "tbrooks@trevorism.com")
         )
